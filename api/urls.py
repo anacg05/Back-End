0b3f1889347bc503fdb3a4189afc908bc8ae4391
unknown <ds@ESN501D1278481.educ501.local>
2025-08-14 16:59:22 -0300

    14/08/25

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('autores', AutoresView.as_view())
+    path('autores', AutoresView.as_view()),
+    path('autores/lista', visu_autor)
 ]
 
